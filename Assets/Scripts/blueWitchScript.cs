d83cad13554491f236b89027e020f3d6e74629ad
Koen <koennenoen@gmail.com>
2024-04-10 12:12:55 +0200

    Better attack

--- Assets/Scripts/blueWitchScript.cs
+++ Assets/Scripts/blueWitchScript.cs
@@ -10,6 +10,7 @@ public class blueWitchScript : MonoBehaviour
     public GameObject greenMagicBall;
     public GameObject blueWitch;
     private float shootTimer = 1f;
+    private float shootTimer2 = 1.5f;
     public Vector3 dir;
     public int amount1 = 0;
     public int amount2 = 0;
@@ -22,11 +23,16 @@ public class blueWitchScript : MonoBehaviour
         if (bossMusic.starting)
         {
             shootTimer -= Time.deltaTime;
+            shootTimer2 -= Time.deltaTime;
             if (shootTimer < 0)
             {
                 shootTimer = 0.3f;
                 bullet = (GameObject)GameObject.Instantiate(blueMagicBall, transform.position + new Vector3(0, 0, Random.Range(-2, 2)), transform.rotation);
                 transform.RotateAround(blueWitch.transform.localPosition, Vector3.back, Time.deltaTime * 25000);
+            }
+            if (shootTimer2 < 0)
+            {
+                shootTimer2 = 0.3f;
                 bullet = (GameObject)GameObject.Instantiate(greenMagicBall, transform.position + new Vector3(0, 0, Random.Range(-2, 2)), transform.rotation);
                 transform.RotateAround(blueWitch.transform.localPosition, Vector3.back, Time.deltaTime * 30000);
             }
