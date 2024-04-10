736bf3a49c0157e8ef75cc2993512323c6c7ce6a
Koen <koennenoen@gmail.com>
2024-04-11 00:26:12 +0200

    Almost done with boss two

--- Assets/blackFireScript.cs
+++ Assets/blackFireScript.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class blackFireScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject target;
+    public redWitchScript redWitchScript;
 
+    public float speed = 4f;
+    private float rotateSpeed = 500f;
+
+    public Rigidbody2D rb;
+
+    public AudioSource blueFireSound;
+    public AudioSource redHitSound;
+    private bool isHit = false;
     // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        target = GameObject.Find("redwitch");
+        rb = target.GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        if (isHit)
+        {
+            Vector2 direction = (Vector2)target.transform.position - rb.position;
+
+            direction.Normalize();
+
+            float rotateAmount = Vector3.Cross(direction, transform.up).z;
+
+            rb.angularVelocity = -rotateAmount * rotateSpeed;
+
+            rb.velocity = transform.up * speed;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isHit = true;
+            //redHitSound.Play();
+        }
+        if (collision.gameObject.layer.Equals("RedWitch"))
+        {
+            AudioSource.PlayClipAtPoint(redHitSound.clip, this.gameObject.transform.position);
+            Object.Destroy(this.gameObject, 1f);
+        }
     }
 }
